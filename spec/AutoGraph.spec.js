6a61d9f7114146378463a82b59e7dde05b92ed17
joe beuckman <joebeuckman@gmail.com>
2013-08-24 12:54:14 -0500

    adding travis ci jasmine

--- spec/AutoGraph.spec.js
+++ spec/AutoGraph.spec.js
@@ -1,6 +1,28 @@
-var r = require('requirejs');
+var requirejs = require('requirejs');
 
-define(["src/AutoGraph"], function (AutoGraph) {
+requirejs.config({
+  urlArgs: "bust=" + (new Date()).getTime(),
+  paths: {
+    "underscore": "lib/underscore-min",
+    "jquery": "lib/jquery-1.10.2.min",
+    "backbone": "lib/backbone-min",
+    "d3": "lib/d3.min"
+  },
+  shim: {
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'underscore': {
+      exports: '_'
+    },
+    'd3': {
+      exports: 'd3'
+    }
+  }
+});
+
+requirejs.define(["src/AutoGraph"], function (AutoGraph) {
 
   describe("AutoGraph", function () {
 
