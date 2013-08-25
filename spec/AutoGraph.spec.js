ef4dca2dd31130e10c7bad1f957a8fbf8e006b26
joe beuckman <joebeuckman@gmail.com>
2013-08-24 20:21:29 -0500

    sending require command to jasmine-node

--- spec/AutoGraph.spec.js
+++ spec/AutoGraph.spec.js
@@ -1,41 +1,12 @@
-var requirejs = require('requirejs');
+define(["AutoGraph"], function (AutoGraph) {
 
-requirejs.config({
-  urlArgs: "bust=" + (new Date()).getTime(),
-  paths: {
-    "underscore": "lib/underscore-min",
-    "jquery": "lib/jquery-1.10.2.min",
-    "backbone": "lib/backbone-min",
-    "d3": "lib/d3.min"
-  },
-  shim: {
-    'backbone': {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    },
-    'underscore': {
-      exports: '_'
-    },
-    'd3': {
-      exports: 'd3'
-    }
-  }
-});
-
-requirejs.define(["src/AutoGraph"], function (AutoGraph) {
-
-  describe("AutoGraph", function () {
-
-
+    describe("AutoGraph", function () {
     it("instantiates an object", function () {
 
       var a = AutoGraph();
-
       expect(a).not.toEqual(null);
 
     });
-
-
   });
 
 });
---
+++ spec/runner.js
@@ -0,0 +1,64 @@
+requirejs.config({
+  urlArgs: "bust=" + (new Date()).getTime(),
+  baseUrl: "../src/",
+  paths: {
+
+    "jquery": "lib/jquery-1.10.2.min",
+    "jasmine": 'lib/jasmine/jasmine',
+    "jasmine-html": 'lib/jasmine/jasmine-html',
+    "spec": '../spec/',
+
+    "underscore": "lib/underscore-min",
+    "backbone": "lib/backbone-min",
+    "d3": "lib/d3.min"
+  },
+  shim: {
+
+    jasmine: {
+      exports: 'jasmine'
+    },
+    'jasmine-html': {
+      deps: ['jasmine'],
+      exports: 'jasmine'
+    },
+
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'underscore': {
+      exports: '_'
+    },
+    'd3': {
+      exports: 'd3'
+    }
+  }
+});
+
+
+require(['jquery', 'jasmine-html'], function ($, jasmine) {
+
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+
+  var htmlReporter = new jasmine.HtmlReporter();
+
+  jasmineEnv.addReporter(htmlReporter);
+
+  jasmineEnv.specFilter = function (spec) {
+    return htmlReporter.specFilter(spec);
+  };
+
+  var specs = [];
+
+  specs.push('AutoGraph.spec');
+
+
+
+  $(function () {
+    require(specs, function (spec) {
+      jasmineEnv.execute();
+    });
+  });
+
+});
