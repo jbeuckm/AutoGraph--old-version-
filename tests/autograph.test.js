904811be4b58f3aa06e0939e0121d4366e78826c
joe beuckman <joebeuckman@gmail.com>
2013-08-29 22:15:49 -0500

    trivial test

--- tests/autograph.test.js
+++ tests/autograph.test.js
@@ -1,5 +1,11 @@
 
-chai.expect('hello').to.be.a('string');
-chai.assert.notEqual(1, null, "autograph not defined");
+require(['autograph'], function (AutoGraph) {
+  var a = autograph('container', 'components.json', '../');
+
+  chai.expect('hello').to.be.a('string');
+  chai.assert.notEqual(1, null, "autograph not defined");
+
+});
+
 
 
