21621ca70fd8cbad20af7099fe9abfc2d35daadb
joe beuckman <joebeuckman@gmail.com>
2013-08-29 22:37:14 -0500

    trivial test

--- tests/autograph.test.js
+++ tests/autograph.test.js
@@ -1,12 +1,15 @@
-
+var expect = chai.expect;
 
 describe('AutoGraph', function() {
 
-    it('should run a trivial test', function() {
+  it('should define the var autograph', function() {
+    expect(autograph).to.not.equal(null);
+  });
 
-      chai.expect(autograph).to.not.equal(null);
-
-    });
+  it('can instantiate a BaseComponent', function(){
+    var bc = new BaseComponent();
+    expect(bc).to.not.equal(null);
+  });
 
 });
 
