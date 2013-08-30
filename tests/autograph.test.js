523892d05b34bfeea0e595b89cb8f56bcafea231
joe beuckman <joebeuckman@gmail.com>
2013-08-29 22:30:17 -0500

    trivial test

--- tests/autograph.test.js
+++ tests/autograph.test.js
@@ -4,11 +4,7 @@ describe('AutoGraph', function() {
 
     it('should run a trivial test', function() {
 
-      require(['autograph'], function (AutoGraph) {
-        var a = autograph('container', 'components.json', '../');
-
-        chai.expect('hello').to.be.a('string');
-      });
+      chai.expect('hello').to.be.a('string');
 
     });
 
