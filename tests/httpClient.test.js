5fc876c547fba67b985a5c7b5d6f292050e72b6c
joe beuckman <joebeuckman@gmail.com>
2013-09-21 12:35:00 -0500

    established a test for the http client

---
+++ tests/httpClient.test.js
@@ -0,0 +1,43 @@
+var expect = chai.expect;
+
+describe('HTTPClient', function() {
+
+  var a;
+  var HTTPClientComponent = {
+    "name": "base components",
+    "model": "HTTPClientComponent",
+    "view": "HTTPClientComponentView"
+  };
+  var loadedComponent;
+
+
+  before(function(){
+    a = new autograph('container', 'components.json', '../');
+  });
+
+  describe('creates an http component', function(){
+
+    before(function(ready){
+      a.componentLibrary.loadComponentClasses(HTTPClientComponent, function(c){
+        loadedComponent = c;
+        ready();
+      });
+    });
+
+    it('makes an http request', function(done) {
+
+      var http = a.placeNewModel(loadedComponent.modelClass, loadedComponent.viewClass, {x:380, y:230});
+
+      var statusOutput = http.outputs['status'].model;
+
+      statusOutput.on("bang", done);
+      http.receiveBang();
+    });
+
+  });
+
+
+});
+
+
+
