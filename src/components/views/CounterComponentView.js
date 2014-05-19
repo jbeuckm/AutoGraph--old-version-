3c9c04a3f7017ad70ac719bca84be1478a3f9964
joe beuckman <joebeuckman@gmail.com>
2014-05-18 19:38:21 -0500

    counter working

--- src/components/views/CounterComponentView.js
+++ src/components/views/CounterComponentView.js
@@ -4,16 +4,26 @@
  */
 var CounterComponentView = BaseComponentView.extend({
 
+    initialize: function () {
+        BaseComponentView.prototype.initialize.call(this);
+
+        var self = this;
+
+        this.model.on("change:count", function(){
+console.log("CHANGE");
+//            this.render();
+            self.text.text(self.model.get("count"));
+        });
+    },
+
+
     /**
      * @method
      */
     render: function () {
-
         BaseComponentView.prototype.render.call(this);
 
-        this.text.text(this.model.get("count"))
-
-        this.model.on("change", this.render);
+        this.text.text(this.model.get("count"));
     }
 
 });
