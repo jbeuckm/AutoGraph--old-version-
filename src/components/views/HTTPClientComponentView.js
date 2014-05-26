d6303a807d9de0b7425f2fdb124eb048dfadd430
joe beuckman <joebeuckman@gmail.com>
2014-05-26 18:54:55 -0500

    select changes http method

--- src/components/views/HTTPClientComponentView.js
+++ src/components/views/HTTPClientComponentView.js
@@ -9,13 +9,18 @@ var HTTPClientComponentView = WebviewComponentView.extend({
 
         WebviewComponentView.prototype.addContent.call(this);
 
+        var m = this.model;
+
         this.htmlHolder
             .attr("width", 100)
             .attr("height", 20);
 
         this.select = this.htmlBody
             .append("select")
-            .attr("class", "nodrag");
+            .attr("class", "nodrag")
+            .on("change", function(e){
+                m.set("method", this.value);
+            });
 
         this.select.selectAll("option")
             .data(["GET", "POST", "PUT", "DELETE"])
