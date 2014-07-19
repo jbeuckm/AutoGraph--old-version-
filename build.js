9d17864218f44bf3ba94c9928438975a0c4c5c6f
joe beuckman <joebeuckman@gmail.com>
2014-07-18 22:04:10 -0500

    updated build with q in require

--- build.js
+++ build.js
@@ -1,29 +1,33 @@
 ({
-  baseUrl:"./src/",
-  name:"main",
-  paths:{
-    requireLib:'lib/require',
-    "underscore":"lib/underscore-min",
-    "jquery":"lib/jquery-1.10.2.min",
-    "backbone":"lib/backbone-min",
-    "d3":"lib/d3.min"
-  },
-  shim:{
-    'backbone':{
-      deps:['underscore', 'jquery'],
-      exports:'Backbone'
+    baseUrl: "./src/",
+    name: "main",
+    paths: {
+        requireLib: 'lib/require',
+        "underscore": "lib/underscore-min",
+        "jquery": "lib/jquery-1.10.2.min",
+        "backbone": "lib/backbone-min",
+        "d3": "lib/d3.min",
+        "q": "lib/q"
     },
-    'underscore':{
-      exports:'_'
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'd3': {
+            exports: 'd3'
+        },
+        'q': {
+            exports: 'Q'
+        }
     },
-    'd3':{
-      exports:'d3'
+    out: "build/autograph-min.js",
+    include: ["requireLib"],
+    wrap: {
+        start: "",
+        end: ""
     }
-  },
-  out:"build/autograph-min.js",
-  include:["requireLib"],
-  wrap:{
-    start:"",
-    end:""
-  }
 })
--- tests/test-config.js
+++ tests/test-config.js
@@ -1,34 +1,33 @@
-
 requirejs.config({
-  urlArgs: "bust=" + (new Date()).getTime(),
-  baseUrl: "../src/",
-  paths: {
-    "underscore": "lib/underscore-min",
-    "jquery": "lib/jquery-1.10.2.min",
-    "backbone": "lib/backbone-min",
-    "d3": "lib/d3.min",
-    "q": "lib/q"
-  },
-  shim: {
-    'backbone': {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
+    urlArgs: "bust=" + (new Date()).getTime(),
+    baseUrl: "../src/",
+    paths: {
+        "underscore": "lib/underscore-min",
+        "jquery": "lib/jquery-1.10.2.min",
+        "backbone": "lib/backbone-min",
+        "d3": "lib/d3.min",
+        "q": "lib/q"
     },
-    'underscore': {
-      exports: '_'
-    },
-    'd3': {
-      exports: 'd3'
-    },
-      'q': {
-          exports: 'Q'
-      }
-  }
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'd3': {
+            exports: 'd3'
+        },
+        'q': {
+            exports: 'Q'
+        }
+    }
 });
 
 require(['autograph'], function (AutoGraph) {
 
-  window.autograph = AutoGraph;
-  console.log("Initialized AutoGraph()");
+    window.autograph = AutoGraph;
+    console.log("Initialized AutoGraph()");
 
 });
